3f8c46289df8a13794d13b1b5dd7bc2c7961828a
mateenshamsi <matinshamsi16@gmail.com>
2024-03-25 13:44:48 +0530

    frontend initialized

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="bg-gray-400 text-gray-200 p-3 mt-auto">
+      <footer className="text-center">Footer</footer>  
+    </div>
+  );
+}
+
+export default Footer;
+
+
+
---
+++ frontend/src/pages/Privacypolicy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Privacypolicy() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Privacypolicy
